d848c8a57528a5676f1e4e6ad23788badae1cbe9
Takashi Yoshida <takashi@takashiyoshida.org>
2026-02-20 08:15:24 +0800

    Add comments

--- keyboards/keychron/q10_max/ansi_encoder/keymaps/takashiyoshida/keymap.c
+++ keyboards/keychron/q10_max/ansi_encoder/keymaps/takashiyoshida/keymap.c
@@ -27,6 +27,7 @@
 
 // Mod-Tap: Tap for KC_F24 and hold for KC_RSFT
 // This is used only for macOS to switch input languages (e.g., English and Japanese)
+// For this change to be effective, Karabiner-Elements must be configured to translate F24 to Globe (fn) key.
 #define MT_RS24 RSFT_T(KC_F24) // MT(MOD_LSFT | MOD_RSFT, KC_F24)
 
 // Home Row Mod
@@ -98,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //         _______,    _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,             _______,            _______,
     //         _______,    _______,            _______,  _______,  _______,  _______,   BAT_LVL,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,   _______,  _______,  _______,
     //         _______,    _______,  _______,            _______,  _______,  _______,                       KC_ENT,             _______,  _______,             _______,  _______,  _______),
-    
+
     [WIN_FN] = LAYOUT_ansi_90(
         RGB_TOG,    _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,   KC_VOLU,  _______,            _______,
         _______,    _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,   _______,  _______,  _______,  _______,  _______,  _______,   _______,  KC_DEL,             KC_HOME,
@@ -122,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MC_3,       MT_CTES,  MT_CTLA,  MT_OPTS,  MT_CMDD,  MT_SFTF,  KC_G,      KC_H,     MT_SFTJ,  MT_CMDK,  MT_OPTL,  MT_CTLS,  KC_QUOT,            KC_ENT,             MO(FN2),
         MC_4,       KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  MT_RS24,  KC_UP,
         MC_5,       MO(MAC_FN), KC_LOPT,          KC_LCMMD, KC_SPC,  MO(MAC_FN),                     KC_SPC,             KC_RCMMD, KC_ROPT,            KC_LEFT,  KC_DOWN,  KC_RGHT),
-            
+
     [WIN_HRMD] = LAYOUT_ansi_90(
         KC_MUTE,    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_F13,             KC_F14,
         MC_1,       KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
